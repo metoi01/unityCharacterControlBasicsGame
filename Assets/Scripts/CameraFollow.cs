51de567b0e427673ffe9b92536c72b6bc6dc2900
Mert Okhan İbrahimağaoğlu <mertokhani@gmail.com>
2024-09-25 23:23:14 +0300

    update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    private Vector3 offset;
+    private Vector3 _offset;
     void Start()
     {
-        offset = transform.position - target.position;
+        _offset = transform.position - target.position;
     }
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, _offset.z + target.position.z);
         transform.position = newPosition;
     }
 }
